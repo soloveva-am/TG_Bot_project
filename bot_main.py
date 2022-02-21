426b91d7fadfccdd41a0435843e81b259c5094e6
anna <soloveva.am@phystech.edu>
2022-02-21 18:33:49 +0300

    старт бота

--- bot_main.py
+++ bot_main.py
@@ -1,5 +1,21 @@
-import aiogram
 
-test
-test
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
+from config import TOKEN
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+Command_info='у меня есть команды: \n \start - приветствие \n \help - помощь \n'#\login \n \\register \n '
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\n я - досуговый бот МФТИ!\n я помогу тебе не заскучать и не выгореть! \n", Command_info)
+
+@dp.message_handler(commands=['help'])
+async  def help_command(message: types.Message):
+    await message.reply(Command_info)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
